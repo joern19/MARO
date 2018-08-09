ad624b64a9999554dd0d13afb4a21cf7fee64221
BuildTools <unconfigured@null.spigotmc.org>
2018-08-09 20:36:29 +0200

    fixed sound bug

--- MARO-aMythicalJourney/src/Manager/SoundManager.java
+++ MARO-aMythicalJourney/src/Manager/SoundManager.java
@@ -11,67 +11,68 @@ import javax.sound.sampled.DataLine;
 
 public class SoundManager {
 
-	private static SoundManager instance;
-	private static HashMap<Sound, AudioInputStream> list;
-	
-	
-	public static SoundManager getInstance() {
-	
-		if(instance == null) {
-			instance = new SoundManager();
-		}
-	
-		return instance;
-		
-	}
-	
-	public SoundManager() {
-		list = new HashMap<>();
-	}
-	
-	
-	private void loadSound(Sound sound) {
-		if(!list.containsValue(sound)) {
-			try {
-				AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(sound.getPath()));
-				list.put(sound, audioInputStream);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}			
-		}
-	}
-	 
-	public void playSound(Sound sound) {
-		try {
-			AudioFormat af = list.get(sound).getFormat();
-			int size = (int) (af.getFrameSize() * list.get(sound).getFrameLength());
-			byte[] audio = new byte[size];
-			DataLine.Info info = new DataLine.Info(Clip.class, af, size);
-			list.get(sound).read(audio, 0, size);
-			Clip clip = (Clip) AudioSystem.getLine(info);
-			clip.open(af, audio, 0, size);
-			clip.start();			
-		}catch(Exception ex) {
-			loadSound(sound);
-			playSound(sound);
-		} 
-	}
+    private static SoundManager instance;
+    private static HashMap<Sound, Clip> list;
 
-	public enum Sound {
-		
-		TEST("src/Sounds/glassbell.wav"),
-		TEST2("src/Sounds/chipquest.wav");
-		
-		String path;
-		
-		Sound(String path) {
-			this.path = path;
-		}
-		
-		public String getPath(){
-			return path;
-		}
-		
-	}
-	
+    public static SoundManager getInstance() {
+
+        if (instance == null) {
+            instance = new SoundManager();
+        }
+
+        return instance;
+
+    }
+
+    public SoundManager() {
+        list = new HashMap<>();
+    }
+
+    private void loadSound(Sound sound) {
+        if (!list.containsKey(sound)) {
+            try {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(sound.getPath()));
+                AudioFormat af = audioInputStream.getFormat();
+                int size = (int) (af.getFrameSize() * audioInputStream.getFrameLength());
+                byte[] audio = new byte[size];
+                DataLine.Info info = new DataLine.Info(Clip.class, af, size);
+                audioInputStream.read(audio, 0, size);
+                Clip clip = (Clip) AudioSystem.getLine(info);
+                clip.open(af, audio, 0, size);
+                list.put(sound, clip);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void playSound(Sound sound) {
+        try {
+            if (!list.containsKey(sound)) {
+                loadSound(sound);
+            }
+            Clip clip = list.get(sound);
+            clip.setMicrosecondPosition(0);
+            clip.start();
+        } catch (Exception ex) {
+            loadSound(sound);
+            playSound(sound);
+        }
+    }
+
+    public enum Sound {
+
+        TEST("src/Sounds/glassbell.wav"),
+        TEST2("src/Sounds/chipquest.wav");
+
+        String path;
+
+        Sound(String path) {
+            this.path = path;
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
 }
