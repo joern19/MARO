da6001f532a5bb462178876339e47ec9453cbcca
Tom <My@gmail.com>
2018-09-21 19:38:57 +0200

    Chatfilter main added

--- Regex/src/de/Chatfilter/Main/Chatfilter.java
+++ Regex/src/de/Chatfilter/Main/Chatfilter.java
@@ -1,5 +1,26 @@
 package de.Chatfilter.Main;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Chatfilter {
 
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		final String string = scanner.nextLine();
+		
+		Pattern pattern = Pattern.compile("");
+		Matcher matcher = pattern.matcher(string);
+		
+		if(matcher.find()) {
+			
+			System.out.println("Ich habe etwas gefunden!");
+			System.out.println(matcher.group());
+			
+		}
+		
+	}
+	
 }
